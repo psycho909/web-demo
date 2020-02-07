6d9561ff0527cf30defb1434ddea028c0e5e85ba
psycho909 <psychosocial909@gmail.com>
2020-02-07 18:24:34 +0800

    123

--- js/IEPlacehoder.js
+++ js/IEPlacehoder.js
@@ -27,3 +27,23 @@ function add_placeholder (id){
 
 // Add right before </body> or inside a DOMReady wrapper
 add_placeholder('myInputField');
+
+function add_placeholder2(clas,color) {
+    var el = document.getElementsByClassName(clas);
+
+    for (var i = 0, len = el.length; i < len; i++) {
+        el[i].onfocus = function () {
+            if (this.value == this.getAttribute("placeholder")) {
+                this.value = "";
+                this.style.cssText = "";
+            }
+        }
+        el[i].onblur = function () {
+            if (this.value.length == 0) {
+                this.value = this.getAttribute("placeholder");
+                this.style.cssText = 'color:'+color+';';
+            }
+        }
+        el[i].onblur();
+    }
+}
