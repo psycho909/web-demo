bf45194a4f20202f59c16c2e07f29b7735873852
psycho909 <psychosocial909@gmail.com>
2020-02-24 18:10:59 +0800

    123

---
+++ js/select.js
@@ -0,0 +1,26 @@
+var selectStyle=(function(){
+    var selectStyles=function(fix,reveal){
+        this.fix=fix;
+        this.reveal=reveal;
+        this.countOption=$(this.fix).find('option').length;
+    }
+    selectStyles.prototype.init=function(){
+        if ($(this.fix).find('option[selected]').length === 1) {
+            $(this.reveal).find('p').html($(this.fix).find('option[selected]').html());
+        } else {
+            $(this.reveal).find('p').html($(this.fix).find('option:first-child').html());
+        }
+        var _this=this;
+        $(this.fix).on("change",function(){
+            var newValue = $(this).val();
+            var newHTML = $(this).find(":selected").html();
+            $(_this.reveal).find('p').html(newHTML);
+        })
+    }
+    var init=function(fix,reveal){
+        new selectStyles(fix,reveal).init();
+    }
+    return {
+        init:init
+    }
+})()
