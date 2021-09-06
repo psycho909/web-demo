d3da7a0aef5ba663cf1b346a9f3e8f314eeec11d
psycho909 <psychosocial909@gmail.com>
2021-09-07 07:48:20 +0800

    123123

--- webpack/src/js/index.js
+++ webpack/src/js/index.js
@@ -1,7 +1,13 @@
-import "../css/default.scss";
+import "../css/index.css";
 const func = () => {
 	return [1, 2];
 };
 const [a, b] = func();
 console.log(a, b);
 console.log("123123a2sd1");
+const p = (rs) => {
+	return new Promise((resolve, reject) => {
+		return resolve(rs);
+	});
+};
+p(123).then((data) => console.log(data));
