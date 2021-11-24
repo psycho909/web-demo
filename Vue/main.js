fb5d4ee03a96bee4c314e05f896cc19a8b3571df
bbb <bbbb>
2021-11-24 18:11:50 +0800

    2123123213

--- Vue/main.js
+++ Vue/main.js
@@ -1,8 +1,16 @@
 import * as child from "./Child.js";
+import Name from "./Name.js";
 var app = new Vue({
 	el: "#app",
 	components: { Child1: child.Child1 },
 	data: {
-		msg: "Hello",
+		msg: "Hello"
 	},
+	methods: {
+		getRef() {
+			console.log(this.$refs.child.data);
+		}
+	}
 });
+
+console.log(Name);
