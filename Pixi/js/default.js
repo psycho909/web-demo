2fa1fb06c54746c15b52f95659c788ed05e5eb6a
bbb <bbbb>
2022-01-06 18:12:39 +0800

    sadasd

---
+++ Pixi/js/default.js
@@ -0,0 +1,469 @@
+var defaultObj={
+    addClass:"default",
+    hasCloseBtn: true,
+    hasActionBtn: true,
+    afterClose:function(){
+        $.gbox.close();
+    },
+    actionBtns: [{
+        text: '關閉',
+        click:function(){
+			$.gbox.close();
+        }
+    }]
+}
+
+var test_data=[
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十十一"},
+	{"account":"一二三四五六七八九十十一十二"},
+	{"account":"一二三四五六七八九十十一十二十三"},
+	{"account":"一二三四五六七八九十十一十二十三十四"},
+	{"account":"一二三四五六七八九十十一十二十三十四一二"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"},
+	{"account":"一二三四五六七八九十"}
+]
+
+// 參加帳號
+var accountObj={
+    addClass:"default",
+    hasCloseBtn: true,
+	hasActionBtn: true,
+	afterOpen:function(){
+		if($(".account-list").length){
+			$(".account-list").mCustomScrollbar({
+				theme:"light"
+			});
+		}
+	},
+    afterClose:function(){
+        $.gbox.close();
+	},
+	actionBtns: [{
+        text: '確定',
+        class:"gbox-success",
+        click:function(){
+			$.gbox.close();
+        }
+    }]
+}
+
+function accountHTMLRender(data){
+	var liHTML='';
+	for(var i=0;i<data.length;i++){
+		liHTML+='<li class="account-list__li">\
+			<label class="account-list__label">\
+				<input class="account-list__radio" type="radio" name="account" value="'+data[i].account+'" />\
+				<span class="account-list__style"></span>\
+				<span class="account-list__text">'+data[i].account+'</span>\
+			</label>\
+		</li>';
+	}
+
+	var accountHTML=
+    '<div class="account-title">請選擇要參加跑跑金手指的遊戲帳號</div>\
+	<ul class="account-list">'+liHTML+'</ul>\
+	<div class="account-notice-box">\
+		<p class="account-notice__title">※注意事項：</p>\
+		<p>虛寶將會發送至選定的遊戲帳號內，</p>\
+		<p>選擇後將不可再修改。</p>\
+	</div>\
+	';
+	return accountHTML;
+}
+
+// $.gbox.open(accountHTMLRender(test_data),accountObj);
+
+// 確認參加帳號
+var checkAccountObj={
+    addClass:"default",
+    hasCloseBtn: true,
+    hasActionBtn: true,
+    afterClose:function(){
+        $.gbox.close();
+	},
+	actionBtns: [{
+        text: '我在想想',
+        class:"gbox-thinkabout",
+        click:function(){
+			$.gbox.close();
+        }
+    },{
+        text: '確定',
+        class:"gbox-success",
+        click:function(){
+			$.gbox.close();
+        }
+    }]
+}
+
+function checkAccountHTMLRender(data){
+	var checkAccountHTML=
+    '<div class="account-text">確定要使用</div>\
+	<div class="account-account">'+data+'</div>\
+	<div class="account-text account-pb">參加嗎?</div>\
+	<div class="account-notice-box">\
+		<p class="account-notice__title">※注意事項：</p>\
+		<p>虛寶將會發送至選定的遊戲帳號內，</p>\
+		<p>選擇後將不可再修改。</p>\
+	</div>\
+	';
+	return checkAccountHTML;
+}
+
+// $.gbox.open(checkAccountHTMLRender("一二三四五六七八九十十一十二十三十四一二"),checkAccountObj);
+
+// 確認參加帳號
+var dontAccountObj={
+    addClass:"default",
+    hasCloseBtn: true,
+    hasActionBtn: true,
+    afterClose:function(){
+        $.gbox.close();
+	},
+	actionBtns: [{
+        text: '確定',
+        class:"gbox-success",
+        click:function(){
+			$.gbox.close();
+        }
+    }]
+}
+
+function doneAccountHTMLRender(data){
+	var doneAccountHTML=
+    '<div class="account-text">已完成參加!</div>\
+	';
+	return doneAccountHTML;
+}
+
+// $.gbox.open(doneAccountHTMLRender("一二三四五六七八九十十一十二十三十四一二"),dontAccountObj);
+
+
+var indexObj={
+    addClass:"gbox-index",
+    hasCloseBtn: true,
+	hasActionBtn: true,
+	afterOpen:function(){
+		if($(".week-100list").length){
+			$(".week-100list").mCustomScrollbar({
+				theme:"light"
+			});
+		}
+		if($(".week-mylist").length){
+			$(".week-mylist").mCustomScrollbar({
+				theme:"light"
+			});
+		}
+	},
+    afterClose:function(){
+        $.gbox.close();
+    },
+    actionBtns: [{
+        text: '關閉',
+        click:function(){
+			$.gbox.close();
+        }
+    }]
+}
+
+// 遊戲說明
+var gameInfo_data=[
+	"玩法：點擊N<span>2</span>0消滅角色並累積分數。",
+	"每日參與次數不限。",
+	"每週五00:00前100名排行重置。",
+	"虛寶獎勵將於2小時內發放至遊戲內禮物盒。",
+	"每個級距的獎勵每週僅能領取一次。",
+	"徽章一個遊戲帳號僅能領取一次。",
+	"徽章獎勵將於每週五結算後發放"
+];
+
+function gameInfoHTMLRender(data){
+	var liHTML="";
+	for(var i=0;i<data.length;i++){
+		liHTML+='<li class="gameinfo-list__li">'+data[i]+'</li>';
+	}
+
+	var gameInfoHTML=
+	'<div class="gameinfo-title">遊戲說明</div>\
+	<ol class="gameinfo-list">'+liHTML+'</ol>\
+	';
+	return gameInfoHTML;
+}
+
+// $.gbox.open(gameInfoHTMLRender(gameInfo_data),indexObj);
+
+// 獎勵清單
+var rewardListObj={
+    addClass:"gbox-index",
+    hasCloseBtn: true,
+	hasActionBtn: true,
+	afterOpen:function(){
+		if($(".week-100list").length){
+			$(".week-100list").mCustomScrollbar({
+				theme:"light"
+			});
+		}
+		if($(".week-mylist").length){
+			$(".week-mylist").mCustomScrollbar({
+				theme:"light"
+			});
+		}
+	},
+    afterClose:function(){
+        $.gbox.close();
+    },
+    actionBtns: [{
+		text: '前往爭霸戰',
+		class:"goto",
+        click:function(){
+			$.gbox.close();
+        }
+    }]
+}
+
+var rewardList_data=[
+	{"lv":"一","point":"25,000","img":"images/reward-1.png","reward":"<span>尼歐的新品車款</span><span>齒輪7代2個</span>"},
+	{"lv":"二","point":"100,000","img":"images/reward-2.png","reward":"<span>黃金齒輪2個</span>"},
+	{"lv":"三","point":"200,000","img":"images/reward-3.png","reward":"<span>鈦金齒輪3個</span>"},
+	{"lv":"四","point":"455,000","img":"images/reward-4.png","reward":"<span>跑跑金手指</span><span>專屬徽章</span>"},
+];
+
+function rewardListHTMLRender(data){
+	var trHTML="";
+	for(var i=0;i<data.length;i++){
+		trHTML+='<tr><td>'+data[i].lv+'</td><td>'+data[i].point+'</td><td><div class="rewardlist-table__reward rewardlist-table__reward-'+(i+1)+'"><img src="'+data[i].img+'" alt=""/><div class="rewardlist-table__reward-item">'+data[i].reward+'</div></div></td></tr>';
+	}
+
+	var rewardListHTML=
+	'<div class="rewardlist-title">獎勵清單</div>\
+	<table class="rewardlist-table">\
+		<thead class="rewardlist-table__thead">\
+			<tr><th>獎勵級距</th><th>分數</th><th>獎勵</th></tr>\
+		</thead>\
+		<tbody class="rewardlist-table__tbody">'+trHTML+'</tbody>\
+	</table>\
+	<div class="rewardlist-notice-box">\
+		<p>*虛寶獎勵將於2小時後發放至遊戲內禮物盒。</p>\
+		<p>*每個級距的獎勵每週僅能領取一次。</p>\
+		<p>*徽章一個遊戲帳號僅能領取一次。</p>\
+		<p>*徽章獎勵將於每週五結算後發放</p>\
+		<p>*參加金手指的抽獎獎勵：睏寶實體娃娃*10名、舒適SR模型*10名</p>\
+		<p>*分數達72萬(含)以上再抽現金10,000元*1名</p>\
+		<p>*分數達42萬(含)以上再抽2,000樂豆點*5名</p>\
+		<p>*分數達18萬(含)以上再抽1,000樂豆點*10名</p>\
+		<p>*報名參加金手指SHOW還有機會再抱走現金<span>10,000</span>元!</p>\
+	</div>\
+	';
+	return rewardListHTML;
+}
+
+// $.gbox.open(rewardListHTMLRender(rewardList_data),rewardListObj);
+
+
+var week100_data={
+	"data":{
+		"total":"12345",
+		"account":"一二三四五六七八九十一二",
+		"score":"1234567"
+	},
+	"listData":[
+		{"rank":"1","account":"一二三四五六一二","score":"2213113"},
+		{"rank":"2","account":"一二三四五六一二","score":"213213"},
+		{"rank":"3","account":"一二三四五六一二","score":"213213"},
+		{"rank":"4","account":"一二三四五六一二","score":"213213"},
+		{"rank":"5","account":"一二三四五六一二","score":"213213"},
+		{"rank":"6","account":"一二三四五六一二","score":"213213"},
+		{"rank":"7","account":"一二三四五六一二","score":"213213"},
+		{"rank":"8","account":"一二三四五六一二","score":"213213"},
+		{"rank":"9","account":"一二三四五六一二","score":"213213"},
+		{"rank":"10","account":"一二三四五一二","score":"213213"},
+	]
+};
+// 活動主畫面_排行榜本週紀錄
+function week100HTMLRender(data){
+	var week100=data.listData;
+	var infoData=data.data;
+
+	var week100li="";
+	for(var i=0;i<week100.length;i++){
+		var score=thu(week100[i].score)
+		week100li+=
+		'<li class="week-100list__li">\
+			<div class="week-100list__rank">\
+				<span class="week-100list__rank-text">第'+week100[i].rank+'名</span>\
+			</div>\
+			<div class="week-100list__info">\
+				<span class="week-100list__info-name">'+week100[i].account+'</span>\
+				<span class="week-100list__info-score">'+score+'</span>\
+			</div>\
+		</li>';
+	}
+	var weekHTML=
+	'<div class="week-title">本週紀錄</div>\
+	<div class="week-list-box">\
+		<div class="week-list--style week-list__total">\
+			<div class="week-list--style-title">總排名</div>\
+			<div class="week-list--style-info">\
+				<span>'+infoData.total+'</span>\
+			</div>\
+		</div>\
+		<div class="week-list--style week-list__player">\
+			<div class="week-list--style-title">玩家名稱</div>\
+			<div class="week-list--style-info">\
+				<span>'+infoData.account+'</span>\
+			</div>\
+		</div>\
+		<div class="week-list--style week-list__top">\
+			<div class="week-list--style-title">最高分數</div>\
+			<div class="week-list--style-info">\
+				<span>'+thu(infoData.score)+'</span>\
+			</div>\
+		</div>\
+	</div>\
+	<div class="week-btn-box">\
+		<a href="javascript:;" class="week-btn__100 btn--on">前100名</a>\
+		<a href="javascript:;" class="week-btn__my btn--off">我的紀錄</a>\
+	</div>\
+	<ul class="week-100list">'+week100li+'</ul>\
+	';
+	return weekHTML;
+}
+
+// $.gbox.open(week100HTMLRender(week100_data),indexObj);
+
+var weekMy_data={
+	"data":{
+		"total":"12345",
+		"account":"一二三四五六七八九十一二",
+		"score":"1234567"
+	},
+	"listData":[
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+		{"score":"213213"},
+	]
+};
+
+// 活動主畫面_排行榜我的紀錄
+function weekMyHTMLRender(data){
+	var weekMy=data.listData;
+	var infoData=data.data;
+
+	var weekmyli="";
+	for(var i=0;i<weekMy.length;i++){
+		var score=thu(weekMy[i].score)
+		weekmyli+=
+		'<li class="week-mylist__li"><div class="week-mylist__img"><span>'+(i+1)+'</div></span><span class="week-mylist__score">'+score+'</span></li>';
+	}
+	var weekHTML=
+	'<div class="week-title-my">我的紀錄</div>\
+	<div class="week-list-box">\
+		<div class="week-list--style week-list__total">\
+			<div class="week-list--style-title">總排名</div>\
+			<div class="week-list--style-info">\
+				<span>'+infoData.total+'</span>\
+			</div>\
+		</div>\
+		<div class="week-list--style week-list__player">\
+			<div class="week-list--style-title">玩家名稱</div>\
+			<div class="week-list--style-info">\
+				<span>'+infoData.account+'</span>\
+			</div>\
+		</div>\
+		<div class="week-list--style week-list__top">\
+			<div class="week-list--style-title">最高分數</div>\
+			<div class="week-list--style-info">\
+				<span>'+thu(infoData.score)+'</span>\
+			</div>\
+		</div>\
+	</div>\
+	<div class="week-btn-box">\
+		<a href="javascript:;" class="week-btn__100 btn--off">前100名</a>\
+		<a href="javascript:;" class="week-btn__history btn--on">歷史紀錄</a>\
+	</div>\
+	<ul class="week-mylist">'+weekmyli+'</ul>\
+	';
+	return weekHTML;
+}
+
+// $.gbox.open(weekMyHTMLRender(weekMy_data),indexObj);
+
+// 請輸入金手指暱稱
+var nicknameObj={
+    addClass:"default",
+    hasCloseBtn: true,
+    hasActionBtn: true,
+    afterClose:function(){
+        $.gbox.close();
+	},
+	actionBtns: [{
+        text: '確定',
+        class:"gbox-success",
+        click:function(){
+			// 獲取暱稱輸入框的值
+			var nickname=$("#nickname").val();
+			// $.gbox.close();
+        }
+    }]
+}
+
+var nicknameHTML=
+	'<div class="nickname-title">請輸入金手指暱稱</div>\
+	<div class="nickname-input">\
+		<input type="text" id="nickname"/>\
+	</div>\
+	<div class="account-notice-box">\
+		<p>*暱稱確定後不可再更改</p>\
+	</div>\
+	';
+
+// $.gbox.open(nicknameHTML,nicknameObj);
+
+// =========================================================
+// 開啟 遊戲說明
+$(".index-btn__gameinfo").on("click",function(){
+	$.gbox.open(gameInfoHTMLRender(gameInfo_data),indexObj);
+})
+
+// 開啟 獎勵清單
+$(".index-btn__rewardlist").on("click",function(){
+	$.gbox.open(rewardListHTMLRender(rewardList_data),indexObj);
+})
+
+// ===========================================================
+
+// 開啟 排行榜
+$(".index-btn__leaderboard").on("click",function(){
+	$.gbox.open(week100HTMLRender(week100_data),indexObj);
+})
+
+// 開啟 前100名
+$("body").on("click",".week-btn__100",function(){
+	$.gbox.open(week100HTMLRender(week100_data),indexObj);
+})
+
+// 開啟 我的紀錄
+$("body").on("click",".week-btn__my",function(){
+	$.gbox.open(weekMyHTMLRender(weekMy_data),indexObj);
+})
+
+function thu(point){
+	var reg = /(?=(\B\d{3})+$)/g;
+	var point=point+"";
+	return point.replace(reg,",")
+}
---
+++ Pixi/js/game.js
@@ -0,0 +1,575 @@
+// 遊戲畫面_暫停
+var ingameObj={
+    addClass:"ingame",
+    hasCloseBtn: true,
+    hasActionBtn: false,
+    afterClose:function(){
+		$.gbox.close();
+		timeStart();
+		playMusicBg()
+    }
+}
+
+function gamePauseHTMLRender(){
+	var gamePauseHTML=
+	`<div class="gamepause-title btn--yellow">Menu</div>
+	<div class="gamepause-btn-box">
+		<a class="gamepause-btn__holdgame btn--blue" href="javascript:;">繼續遊戲</a>
+		<a class="gamepause-btn__restart btn--blue" href="javascript:;">重新開始</a>
+		<a class="gamepause-btn__backhome btn--blue" href="index.aspx">回到首頁</a>
+	</div>
+	`;
+	return gamePauseHTML;
+}
+
+// 繼續遊戲 事件
+$("body").on("touchstart",".gamepause-btn__holdgame",function(){
+	$.gbox.close();
+	timeStart();
+	playMusicBg()
+})
+// 重新開始 事件
+$("body").on("touchstart",".gamepause-btn__restart",function(){
+	$.gbox.close();
+	restart()
+})
+
+// $.gbox.open(gamePauseHTMLRender(),ingameObj);
+
+// =============================================================================
+
+var canvas={};
+var scale = (window.innerWidth / 768)
+var scalePadding = scale * 38 * 2;
+var scalePadding2 = scale * 38;
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+canvas.itemWidth = 669*scale;
+
+var krArr=[];
+var canvasW = canvas.itemWidth;
+var canvasH = canvas.height;
+// 預設數量
+var initCount=1000;
+var itemW=215*scale;
+var itemH=149*scale;
+var itemCol=3;
+var colWidth=canvasW/itemCol;
+var itemPadding=(colWidth-itemW)/2;
+var itemOffsetLeft=colWidth;
+var itemOffsetTop=itemH;
+var headDiv="";
+var timer=null;
+// 預設時間
+var gameTime=10;
+var speedY=0;
+var jumpStatus=false;
+var jumpControl=false;
+var jumpCount=0;
+var tempY=0;
+
+// 出現隻數
+var shoot=0;
+// 分數統計
+var scoreArr=[];
+
+// 計算成功失敗的狀態
+// touchStatus.success 獲取成功的次數
+// touchStatus.faild 或失敗的次數
+var touchStatus={
+	success:0,
+	faild:0
+}
+// 初始化高度
+$(".game").height(window.innerHeight)
+
+
+// 播放背景音樂
+function playMusicBg(){
+	bgMusic.play()
+}
+
+// 停止背景音樂
+// 20201118 修改
+function stopMusicBg(){
+	bgMusic.stop()
+}
+
+// 暫停背景音樂
+// 20201118 add
+function pauseMusicBg(){
+	bgMusic.pause()
+}
+
+// 時間開始跟結束
+// 20201118 add
+function timeStart(){
+	timer=setInterval(function(){
+		gameTime--;
+		// 當時間結束時
+		if(gameTime <= 0){
+			clearInterval(timer)
+			timeText.text=0;
+			$("#app").prepend("<div class='game-stopbg'></div>")
+			bgMusic.stop()
+			replacePage()
+		}
+		timeText.text=gameTime;
+	},1000)
+}
+
+// 重要
+// 主要修改這個function
+// 在時間倒數完後會使用這個function
+// 遊戲結束跳轉頁面
+function replacePage(){
+	// 放入程式
+	// 獲取 scoreTotal() 分數
+	// 記錄完資料後再切換到結果頁跳出總分
+	// .............................
+	// resultPage()
+}
+
+// 沒破紀錄
+function resultPage(){
+	// 結束時結算頁面跳出 結束文字
+	$(".end-text").addClass("show");
+	// 一秒後跳出結算葉面
+	setTimeout(function(){
+		$(".result").addClass("show")
+	},1200)
+	// 結算分數
+	$(".result-status__score").text(thu(scoreTotal()))
+}
+
+// 有破紀錄
+function resultPageNew(){
+	// 結束時結算頁面跳出 結束文字
+	$(".end-text").addClass("show");
+	// 一秒後跳出結算葉面
+	setTimeout(function(){
+		$(".result").addClass("show")
+		$(".result-status-box").addClass("new")
+	},1200)
+	// 結算分數
+	$(".result-status__score").text(thu(scoreTotal()))
+}
+
+// 計算總分 並回傳分數
+function scoreTotal(){
+	var test=[];
+	for(var i=0;i<scoreArr.length;i++){
+		test.push(scoreArr[i].filter(v=>v)[0])
+	}
+
+	return test.filter(function(v){
+		return v;
+	}).reduce(function(p,n){
+		return p+n.point
+	},0)
+}
+
+function thu(point){
+	var reg = /(?=(\B\d{3})+$)/g;
+	var point=point+"";
+	return point.replace(reg,",")
+}
+
+// 聲音讀取
+Howler.autoUnlock = true;
+var bgMusic = new Howl({
+	src: ['audio/music.mp3'],
+	loop:true,
+	volume:0.1,
+	onplayerror: function() {
+		bgMusic.once('unlock', function() {
+			bgMusic.play();
+		});
+	},
+	onload:function(){
+		bgMusic.once('unlock', function() {
+			bgMusic.play();
+		});
+	}
+});
+var attack = new Howl({
+	src: ['audio/attack.mp3']
+});
+// 20201108 add
+var jumb = new Howl({
+	src: ['audio/jumb.mp3']
+});
+// 圖片Canvas
+let Application = PIXI.Application;
+let loader = PIXI.loader;
+let Sprite = PIXI.Sprite;
+let resources = PIXI.loader.resources;
+let Container = PIXI.Container;
+
+let app = new Application({
+	width: canvas.width,
+	height: canvas.height,
+	transparent: true,
+	antialias:true,
+	resolution:1,
+	roundPixels: true
+});
+app.renderer.view.style.position = "absolute";
+app.renderer.view.width=window.innerWidth;
+app.renderer.view.height=window.innerHeight;
+app.renderer.view.style.top = "0";
+app.renderer.view.style.left = "0";
+document.getElementById("app").appendChild(app.view);
+
+loader
+	.add([
+		{name:"bg",url:"images/game-bg.jpg"},
+		{name: "blue",url: "images/blue.png"},
+		{name: "yellow",url: "images/yellow.png"},
+		{name: "red",url: "images/red.png"},
+		{name: "green",url: "images/green.png"},
+		{name: "pink",url: "images/pink.png"},
+		{name: "btnOff",url: "images/btn.png"},
+		{name: "btnFire",url: "images/fire.png"},
+		{name: "clock",url: "images/clock-bg.png"},
+		{name: "pause",url: "images/pause.png"},
+	])
+	.load(setup);
+
+let head,state,gameScene,gameHead=[],btnGroup=[],timeText;
+var btnOff=PIXI.Texture.from('images/btn.png');
+var btnFire=PIXI.Texture.from('images/fire.png');
+
+
+// canvas初始化
+// 20201118 修改
+function setup() {
+	initKR();
+	var btnWidth=230;
+	var btnHeight=244;
+
+	gameScene=new PIXI.particles.ParticleContainer();
+	app.stage.addChild(gameScene);
+
+	let bg = new Sprite(
+		resources.bg.texture
+	);
+	gameScene.addChild(bg);
+	gameScene.width=768*scale;
+	gameScene.height=1685*scale;
+
+	gameHead=new Container;
+	app.stage.addChild(gameHead);
+
+	gameHead.width=canvas.itemWidth;
+	gameHead.Height=window.innerHeight;
+	// gameHead.y=-(btnHeight*scale/2)-(10*scale);
+	gameHead.y=-btnHeight*scale;
+	gameHead.x=50*scale;
+	for(var i=0;i<krArr.length;i++){
+		for(var j=0;j<krArr[i].length;j++){
+			if(krArr[i][j]){
+				if(i < 30){
+					head = new Sprite(resources.pink.texture);
+				}
+				if(i >= 30){
+					head = new Sprite(resources.yellow.texture);
+				}
+				if(i >= 60){
+					head = new Sprite(resources.blue.texture);
+				}
+				if(i >= 90){
+					head = new Sprite(resources.green.texture);
+				}
+				if(i >= 120){
+					head = new Sprite(resources.red.texture);
+				}
+				head.scale.set(scale,scale);
+				head.x=krArr[i][j].x;
+				head.y=krArr[i][j].y;
+				gameHead.addChild(head);
+			}
+		}
+	}
+
+	var clock = new Sprite(
+		resources.clock.texture
+	);
+	clock.x=scale*14;
+	clock.y=scale*17;
+	clock.scale.set(scale,scale);
+	app.stage.addChild(clock);
+
+	var textBg=new PIXI.Graphics();
+	var textBgWidth=40*scale;
+	textBg.beginFill("0xffffff");
+	textBg.drawCircle(0,0,textBgWidth);
+	textBg.x=textBgWidth+(28*scale);
+	textBg.y=textBgWidth+(57*scale);
+	textBg.endFill();
+	
+	timeText = new PIXI.Text(gameTime,{
+		fontFamily: 'Arial',
+		fontSize:45*scale,
+		align:"center"
+	});
+	timeText.anchor.x=0.5
+	timeText.anchor.y=0.5
+	timeText.x = 0;
+	timeText.y = 0;
+
+	textBg.addChild(timeText)
+	app.stage.addChild(textBg);
+	
+	var pause = new Sprite(
+		resources.pause.texture
+	);
+	pause.buttonMode=true;
+	pause.interactive=true;
+	pause.x=654*scale;
+	pause.y=46*scale;
+	pause.scale.set(scale,scale);
+	
+	pause.on("tap",()=>{
+		timeStop()
+		$.gbox.open(gamePauseHTMLRender(),ingameObj);
+	})
+	app.stage.addChild(pause);
+
+	for(var i=0;i<3;i++){
+		var btn=new Sprite(btnOff); 
+		btn.buttonMode=true;
+		btn.interactive=true;
+		// btn.x=(66*scale)+(211*scale*(i+1))-(btnWidth*scale/2)-(211*scale/2);
+		btn.x=(36*scale)+btnWidth*scale*(i);
+		btn.y=window.innerHeight-(btnHeight*scale)-(60*scale);
+		// btn.scale.set(scale,scale);
+		btn.width=btnWidth*scale;
+		btn.height=btnHeight*scale;
+		btnGroup.push(btn)
+		app.stage.addChild(btnGroup[i]);
+	}
+	btnGroup[0].on("tap",()=>{
+		if(!jumpControl){
+			updateKR(0)
+		}
+	})
+	btnGroup[1].on("tap",()=>{
+		if(!jumpControl){
+			updateKR(1)
+		}
+	})
+	btnGroup[2].on("tap",()=>{
+		if(!jumpControl){
+			updateKR(2)
+		}
+	})
+	countDown();
+	btnCurr()
+}
+
+
+// 時間強制停止
+// 20201118 修改
+function timeStop(){
+	clearInterval(timer)
+	pauseMusicBg()
+}
+
+// 初始化
+function initKR(){
+	// 紀錄成功失敗狀態
+	touchStatus={
+		success:0,
+		faild:0
+	}
+
+	krArr=[];
+	gameTime=30;
+	shoot=0;
+	scoreArr=[];
+	cd=3;
+	if(timeText){
+		timeText.text=gameTime;
+	}
+	for (var i = 0; i < initCount; i++) {
+		addKR(i)
+	}
+}
+
+// 增加KR
+function addKR(z,action) {
+	var kr = Array.from(Array(3), x => false);
+	var r = Math.floor(Math.random() * 3);
+	var itemX = (r * itemOffsetLeft) + itemPadding;
+	// var itemY = canvasH - (itemOffsetTop * z) - itemH - 60;
+	var itemY = canvasH - (itemOffsetTop * z)- itemH;
+	kr[r] = {
+		x: itemX,
+		y: itemY
+	};
+	kr[r].shoot="pink";
+	kr[r].point=2000;
+
+	if(z < 30){
+		kr[r].shoot="pink";
+		kr[r].point=2000;
+	}
+	if(z >= 30){
+		kr[r].shoot="yellow";
+		kr[r].point=4000;
+	}
+	if(z >= 60){
+		kr[r].shoot="blue";
+		kr[r].point=8000;
+	}
+	if(z >= 90){
+		kr[r].shoot="green";
+		kr[r].point=10000;
+	}
+	if(z >= 120){
+		kr[r].shoot="red";
+		kr[r].point=20000;
+	}
+	if(action){
+		kr[r].shoot="red";
+		kr[r].point=20000;
+		head = new Sprite(resources.red.texture);
+		
+		head.width=itemW;
+		head.height=itemH;
+		head.x=kr[r].x;
+		head.y=kr[r].y;
+
+		gameHead.addChild(head);
+		
+	}
+	krArr.push(kr)
+}
+
+// 全部移動和增加新的
+// 20201118 修改
+function updateKR(t) {
+	if (krArr[0][t]) {
+		touchStatus.success++;
+		delKR(krArr[0])
+		gameHead.children.forEach(function(head){
+			head.y+=itemH;
+		})
+		if (krArr.length < 12) {
+			addKR(krArr.length,"add")
+		}
+		btnCurr()
+	}else{
+		// 失敗時
+		touchStatus.faild++;
+		if(!jumpControl){
+			jumb.play()
+			jumpControl=true;
+			btnControl=false;
+			jumpCount=0;
+			tempY=gameHead.children[shoot].y;
+			gameLoop();
+		}
+	}
+}
+
+// 按錯按鈕跳一下
+function gameLoop() {
+	if(jumpControl){
+		gsap.to(gameHead.children[shoot],0.04,{
+			y:gameHead.children[shoot].y-36,
+			repeat:1,
+			yoyo:true,
+			onComplete:function(){
+				jumpControl=false
+			}
+		})
+	}
+}
+
+// 刪除KR
+function delKR(d){
+	scoreArr.push(d);
+	krArr = krArr.slice(1)
+	gameHead.children[shoot].visible=false
+	shoot++;
+	attack.play()
+}
+
+// 倒數計時
+function countDown(){
+	if(!$(".game-cd").length){
+		$("#app").prepend("<div class='game-cd game-cd3'></div>")
+	}
+	if(cd <= 0){
+		$(".game-cd").remove()
+		timeStart()
+		return;
+	}
+	setTimeout(function(){
+		cd--;
+		$(".game-cd").attr("class","game-cd game-cd"+cd)
+		countDown()
+	}, 1000);
+}
+
+// 按鈕判斷噴火樣式
+function btnCurr(){
+	for(var i=0;i<krArr[0].length;i++){
+		if(krArr[0][i]){
+			btnGroup[i].texture=btnFire
+		}else{
+			btnGroup[i].texture=btnOff
+		}
+	}
+}
+
+// 重新開始
+// 20201118 修改
+function restart(){
+	initKR();
+	countDown();
+	btnCurr();
+	stopMusicBg()
+	playMusicBg()
+	for(var i=gameHead.children.length-1;i>=0;i--){
+		gameHead.removeChild(gameHead.children[i]);
+	}
+
+	for(var i=0;i<krArr.length;i++){
+		for(var j=0;j<krArr[i].length;j++){
+			if(krArr[i][j]){
+				if(i < 30){
+					head = new Sprite(resources.pink.texture);
+				}
+				if(i > 30){
+					head = new Sprite(resources.yellow.texture);
+				}
+				if(i > 60){
+					head = new Sprite(resources.blue.texture);
+				}
+				if(i > 90){
+					head = new Sprite(resources.green.texture);
+				}
+				if(i > 120){
+					head = new Sprite(resources.red.texture);
+				}
+				head.width=itemW;
+				head.height=itemH;
+				head.x=krArr[i][j].x;
+				head.y=krArr[i][j].y;
+				gameHead.addChild(head);
+			}
+		}
+	}
+}
+
+
+window.onresize=function(){
+	var w=window.innerWidth;
+	var h=window.innerHeight;
+	
+	app.renderer.resize(w,h)
+}
+
