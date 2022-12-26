710e9c8e8f4d4741a6423096acde776bbaa5e192
Ching CHEN <chingchen@gamania.com>
2022-12-26 18:18:24 +0800

    asdsadasdasdasdasdasdad

--- event/src/js/mCustomScrollbar.js
+++
@@ -1,8 +0,0 @@
-$(".account-list").mCustomScrollbar({
-	theme: "light",
-	contentTouchScroll: true,
-	mouseWheel: {
-					preventDefault: true
-				},
-	advanced: { extraDraggableSelectors: ".account-list" },
-});
--- event/src/js/video.js
+++
@@ -1,8 +0,0 @@
-// 影片 : 播放時事件
-function videoPlaying(video, callback) {
-	$(video)[0].onplaying = callback;
-}
-// 影片 : 播放結束候事件
-function videoEnded(video, callback) {
-	$(video)[0].onended = callback;
-}
