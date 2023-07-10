bac8f846798e20dc44fd7f9fd4c9ad8fe571123b
Ching CHEN <chingchen@gamania.com>
2023-07-10 11:43:02 +0800

    asdasdsadasdsadasd

---
+++ 167.js
@@ -0,0 +1,5 @@
+var people = {
+    name: "Chen",
+    age: 123
+};
+console.log(people);
---
+++ 167.ts
@@ -0,0 +1,11 @@
+interface User {
+	name: string;
+	age: number;
+}
+
+let people: User = {
+	name: "Chen",
+	age: 123
+};
+
+console.log(people);
