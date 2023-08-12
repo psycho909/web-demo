4d2ad2edfb4df09283c7f69b4b7db2afbda3dcf6
psycho909 <psychosocial909@gmail.com>
2023-08-12 18:02:59 +0800

    asdasd

--- GIS/js/default.js
+++ GIS/js/default.js
@@ -1,5 +1,7 @@
 var pMap;
 var MapOptions;
+var messageBox = null;
+// var TViewer = TViewer || new TViewerBase();
 // 初始化地圖
 function InitWnd() {
 	var pOMap = document.getElementById("TGMap");
@@ -22,25 +24,37 @@ function InitWnd() {
 			//(可設定參數有：DEFAULT / HORIZONTAL_BAR / DROPDOWN_MENU)
 		},
 		navigationControl: false, //navigationControl(是否開啟縮放控制列)
-		navigationControlOptions: {
-			//navigationControlOptions(提供指定縮放控制列)
-			controlPosition: TGOS.TGControlPosition.RIGHT_TOP,
-			//controlPosition(設定縮放控制列在地圖的位置)
-			navigationControlStyle: TGOS.TGNavigationControlStyle.SMALL
-			//navigationControlStyle(設定縮放控制列樣式)
-			//(可設定參數有：完整版 / 縮小版(DEFAULT / SMALL))
-		},
 		scaleControl: false, //scaleControl(是否開啟比例尺控制項)
-		scaleControlOptions: {
-			//scaleControlOptions(提供指定比例尺控制項)
-			controlPosition: TGOS.TGControlPosition.LEFT_BOTTOM
-			// controlPosition (設定比例尺控制項在地圖的位置)
-		},
 		draggable: true, //draggable(設定地圖是否可被拖曳)
 		keyboardShortcuts: true //keyboardShortcuts(設定是否可用鍵盤控制地圖)
 	};
 	pMap.setOptions(MapOptions);
 	pMap.setZoom(7); //指定地圖層級
+	$(".scrollMap-zoomText").text(pMap.getZoom());
+	var { x, y } = pMap.getCenter();
+	$(".top-info__map-current-info").text(`${x.toFixed(3)},${y.toFixed(3)}`);
+	var aCurrentScale = (function () {
+		var ps = TGOS.MapTypeScales.EPSG3826.scales;
+		var pslen = ps.length;
+		var mapLevel = parseInt(pMap.getTransformation().getMapLevel(), 10);
+		return ps[pslen - mapLevel - 1];
+	})();
+	$(".top-info__map-scale-info").text(aCurrentScale);
+	TGOS.TGEvent.addListener(pMap, "zoom_changed", function (e) {
+		var aCurrentScale = (function () {
+			var ps = TGOS.MapTypeScales.EPSG3826.scales;
+			var pslen = ps.length;
+			var mapLevel = parseInt(pMap.getTransformation().getMapLevel(), 10);
+			return ps[pslen - mapLevel - 1];
+		})();
+		$(".top-info__map-scale-info").text(aCurrentScale);
+	});
+	TGOS.TGEvent.addListener(pMap, "mousemove", function (e) {
+		//加入滑鼠單擊地圖事件監聽器
+		var pt = e.point; //取得滑鼠點擊位置坐標
+		var { x, y } = pt;
+		$(".top-info__map-current-info").text(`${x.toFixed(3)},${y.toFixed(3)}`);
+	});
 }
 function get_Envelope() {
 	alert(pMap.getBounds()); //取出地圖的邊框Envelope物件
@@ -60,6 +74,96 @@ function MoveMap() {
 	pMap.panBy(X, Y); //輸入坐標x,y，以此點做為平移基準，可對地圖進行平移
 }
 window.addEventListener("wheel", function (e) {
-	console.log(pMap.getZoom());
+	$(".scrollMap-zoomText").text(pMap.getZoom());
 });
-// $("#draggable").draggable();
+
+function makeDraggable(element) {
+	let isDragging = false;
+	let initialX;
+	let initialY;
+	let offsetX = 0;
+	let offsetY = 0;
+
+	const initialLeft = parseInt(getComputedStyle(element).left, 10);
+	const initialTop = parseInt(getComputedStyle(element).top, 10);
+	element.style.left = initialLeft + "px";
+	element.style.top = initialTop + "px";
+
+	// 開始拖曳
+	const startDragging = (e) => {
+		isDragging = true;
+		if (e.type === "touchstart") {
+			initialX = e.touches[0].clientX;
+			initialY = e.touches[0].clientY;
+		} else {
+			initialX = e.clientX;
+			initialY = e.clientY;
+		}
+		offsetX = initialX - element.getBoundingClientRect().left;
+		offsetY = initialY - element.getBoundingClientRect().top;
+		element.style.cursor = "grabbing";
+	};
+
+	// 拖曳中
+	const doDrag = (e) => {
+		if (!isDragging) return;
+
+		e.preventDefault();
+
+		const currentX = e.type === "touchmove" ? e.touches[0].clientX - offsetX : e.clientX - offsetX;
+		const currentY = e.type === "touchmove" ? e.touches[0].clientY - offsetY : e.clientY - offsetY;
+
+		element.style.left = `${currentX}px`;
+		element.style.top = `${currentY}px`;
+	};
+
+	// 停止拖曳
+	const stopDragging = () => {
+		isDragging = false;
+		element.style.cursor = "grab";
+	};
+
+	element.addEventListener("mousedown", startDragging);
+	element.addEventListener("touchstart", startDragging);
+
+	document.addEventListener("mousemove", doDrag);
+	document.addEventListener("touchmove", doDrag, { passive: false });
+
+	document.addEventListener("mouseup", stopDragging);
+	document.addEventListener("touchend", stopDragging);
+}
+
+const boxes = document.querySelectorAll(".fixed-box");
+boxes.forEach(makeDraggable);
+
+$(".menu-item").on("click", function () {
+	let type = $(this).data("target");
+	// $("#" + type).draggable();
+	$("#" + type).show();
+	$(".fixed-box")
+		.not("#" + type)
+		.hide();
+});
+$(".fixed-box__close").on("click", function () {
+	$(this).closest(".fixed-box").hide();
+});
+
+$(".scrollMap-btn").on("click", function () {
+	let type = $(this).data("type");
+	let zoom = pMap.getZoom();
+	if (type === "zoomIn") {
+		zoom = zoom + 1;
+		if (zoom >= 19) {
+			return;
+		}
+		pMap.setZoom(zoom);
+	}
+	if (type === "zoomOut") {
+		zoom = zoom - 1;
+		if (zoom <= 1) {
+			return;
+		}
+		pMap.setZoom(zoom);
+	}
+	$(".scrollMap-zoomText").text(pMap.getZoom());
+});
